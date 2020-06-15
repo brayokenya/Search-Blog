e662082ef6f48cddf22ffd0ac3bb665b5c9f73ad
brayokenya <kiirubrian21@gmail.com>
2020-06-15 16:40:16 +0300

    updated manage.py

--- manage.py
+++ manage.py
@@ -12,6 +12,13 @@ manager.add_command('server',Server)
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+@manager.command
+def test():
+    """
+    Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=5).run(tests) 
 
 
 @manager.shell
